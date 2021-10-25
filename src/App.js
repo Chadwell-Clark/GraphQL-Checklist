9647604434f237efc9aa68408839c4a0a18a6362
Chadwell Clark <wcclark23@comcast.net>
2021-10-24 21:08:29 -0500

    Initial Commit Todos Listing and toggling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
+const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: uuid!, $done: Boolean!) {
+    update_todos(where: { id: { _eq: $id } }, _set: { done: $done }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+//list todos
+//add todos
+//toggle todos
+//delete todos
 
 function App() {
+  const { data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
+
+  async function handleToggleTodo({ id, done }) {
+    const data = await toggleTodo({ variables: { id, done: !done } });
+    console.log(data);
+  }
+
+  if (loading) return <div>Loading Todos</div>;
+  if (error) return <div>Error Fetching Todos</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">
+        GraphQL Checklist{" "}
+        <span role="img" aria-label="Checkmark">
+          ✅
+        </span>
+      </h1>
+      {/* Todo Form */}
+      <form className="mb3">
+        <input
+          className="pa2 f4 b--dashed"
+          type="text"
+          placeholder="Write your todo"
+        />
+        <button className="pa2 f4 bg-green" type="submit">
+          Create
+        </button>
+      </form>
+
+      {/* Todo List */}
+      <div className="flex items-center justify-center flex-column">
+        {data.todos.map((todo) => (
+          <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+            <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>
+              {todo.text}
+            </span>
+            <button className="bg-transparent bn f4">
+              <span className="red">&times;</span>
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
